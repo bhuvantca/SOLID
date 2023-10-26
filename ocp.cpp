008a4f1b1b21a942921bc7976524382fc763a658
bhuvantca <68459580+bhuvantca@users.noreply.github.com>
2023-10-26 11:37:30 +0530

    Create ocp.cpp

---
+++ ocp.cpp
@@ -0,0 +1,4 @@
+Open Closed principles:
+  Open-Closed Principle states that “Software Components (such as classes, modules, function, etc) should be open for extension but close for modifications.”
+  In simple terms, it means that new functionality in the code can be added without changing existing source code.
+
