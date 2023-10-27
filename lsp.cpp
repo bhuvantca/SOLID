762a0e0c85ac6887a030443efec0cfae9cfe06bc
BhuvanBose <68459580+bhuvantca@users.noreply.github.com>
2023-10-27 19:36:50 +0530

    Create lsp.cpp

---
+++ lsp.cpp
@@ -0,0 +1,10 @@
+
+/*
+The principle defines that objects of a superclass shall be replaceable with objects of its subclasses without breaking the application. That requires the objects of your subclasses to behave in the same way as the objects of your superclass.
+  */
+
+/*
+Input Param - We need to implement less restrictive validation rules in subclass.
+Output Param - We can implement strict rules in subclass for defined return value.
+
+*/
