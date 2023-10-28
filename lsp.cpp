42f1ec6d55d6e1f99ddc9b59ec981adabfdb7409
BhuvanBose <68459580+bhuvantca@users.noreply.github.com>
2023-10-27 23:34:36 -0700

    Update lsp.cpp

--- lsp.cpp
+++ lsp.cpp
@@ -1,4 +1,41 @@
 
+#include<iostream>
+
+class Fruit
+{
+  public:
+  virtual void color() = 0;
+};
+
+class Orange : public Fruit
+{
+    public:
+    void color()
+    {
+        std::cout<<"Orange"<<std::endl;
+    }
+};
+
+class Apple : public Fruit
+{
+    public:
+    void color()
+    {
+        std::cout<<"Red"<<std::endl;
+    }
+};
+
+int main()
+{
+    Fruit *orange = new Orange();
+    orange->color();
+    Fruit *apple = new Apple();
+    apple->color();
+    return 0;
+}
+
+
+
 /*
 The principle defines that objects of a superclass shall be replaceable with objects of its subclasses without breaking the application. That requires the objects of your subclasses to behave in the same way as the objects of your superclass.
   */
