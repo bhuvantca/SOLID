668663f0608f1396dcd733e6e456612880e68544
BhuvanBose <68459580+bhuvantca@users.noreply.github.com>
2023-10-30 11:09:20 -0700

    Update ISP.cpp

--- ISP.cpp
+++ ISP.cpp
@@ -9,3 +9,6 @@ Interface Segregation Principle:
   So, Class shouldnot forced to implement a method, which is not used.
 Adv: 1. Increase code reusability 2. Easier to implement.
 */
+/*
+A class must not have to implement any interface which is not required by the class
+*/
