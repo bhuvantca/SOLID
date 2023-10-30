8ce8db09cfe00764b03b961f95765f298d804aef
BhuvanBose <68459580+bhuvantca@users.noreply.github.com>
2023-10-30 10:55:01 -0700

    Update ISP.cpp

--- ISP.cpp
+++ ISP.cpp
@@ -1,1 +1,11 @@
-I
+
+
+
+
+
+/*
+Interface Segregation Principle:
+  ISP goal is splits the larger interfaces into smaller and more specific to requirement.
+  So, Class shouldnot forced to implement a method, which is not used.
+Adv: 1. Increase code reusability 2. Easier to implement.
+*/
